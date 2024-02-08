aa98f43c6a51de7284eb99b047e019fdd68e8bf9
unknown <qpscm662@naver.com>
2024-02-09 02:21:05 +0900

    feat:Joinpage CSS

--- vite-project/src/components/JoinPage.jsx
+++ vite-project/src/components/JoinPage.jsx
@@ -1,48 +1,56 @@
 import React from 'react';
+import * as S from './style/JoinPage.style';
 
 function JoinPage() {
   return (
-    <div>
-      <div>
-        <h2>JOIN</h2>
-        <form>
-          <div>
-            <div>
-              <label>E-mail</label>
-              <input type="email" />@
-              <select name="domain">
+    <S.Section>
+      <S.Parents>
+        <S.Title>JOIN</S.Title>
+        <S.Form>
+          <S.BorderTop>
+            <S.EmailForm>
+              <S.EmailLabel>E-mail</S.EmailLabel>
+              <S.EmailInput type="email" />@
+              <S.EmailSelect name="domain">
                 <option>naver.com</option>
                 <option>gmail.com</option>
                 <option>daum.net</option>
                 <option>nate.com</option>
                 <option>직접입력</option>
-              </select>
-            </div>
-          </div>
-          {/* 유저정보 */}
-          <div>
-            <p>
-              <label>닉네임</label>
-              <input type="text" />
-              <div>닉네임을 입력은 필수입니다.</div>
-            </p>
-          </div>
-          <div>
-            <p>
-              <label>비밀번호</label>
-              <input type="password" />
-              <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
-            </p>
-            <p>
-              <label>비밀번호 확인</label>
-              <input type="password" />
-              <div>비밀번호가 다릅니다.</div>
-            </p>
-          </div>
-          <button>회원가입</button>
-        </form>
-      </div>
-    </div>
+              </S.EmailSelect>
+            </S.EmailForm>
+            <S.ValidationJoin>*이메일을 입력 해주세요.</S.ValidationJoin>
+          </S.BorderTop>
+          {/* 정보입력 */}
+          <S.UserBorder>
+            <S.Block>
+              <S.StyleP>
+                <S.Label>닉네임</S.Label>
+                <S.LabelInput type="text" />
+              </S.StyleP>
+              <S.ValidationJoin>*두 글자 이상 입력 해주세요.</S.ValidationJoin>
+            </S.Block>
+          </S.UserBorder>
+          <S.BorderBottom>
+            <S.Block>
+              <S.StyleP>
+                <S.Label>비밀번호</S.Label>
+                <S.LabelInput type="password" />
+              </S.StyleP>
+              <S.ValidationJoin>
+                *영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
+              </S.ValidationJoin>
+              <S.StyleP>
+                <S.Label>비밀번호 확인</S.Label>
+                <S.LabelInput type="password" />
+              </S.StyleP>
+              <S.ValidationJoin>*비밀번호가 다릅니다.</S.ValidationJoin>
+            </S.Block>
+          </S.BorderBottom>
+          <S.JoinBtn disabled={true}>회원가입</S.JoinBtn>
+        </S.Form>
+      </S.Parents>
+    </S.Section>
   );
 }
 
--- vite-project/src/components/style/JoinPage.style.jsx
+++ vite-project/src/components/style/JoinPage.style.jsx
@@ -1,6 +1,103 @@
 import styled from 'styled-components';
 
-const Parents = styled.div`
-  width: 700px;
+export const Section = styled.section`
+  width: 100%
   font-size: 16px;
+`;
+export const Parents = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  text-align: center;
+`;
+export const Title = styled.h2`
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 54px;
+`;
+export const Form = styled.form`
+  padding: 20px 0;
+`;
+export const BorderTop = styled.div`
+  padding: 20px 0;
+  border-top: 2px solid #333;
+  border-bottom: 1px solid #ccc;
+`;
+export const UserBorder = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid #ccc;
+`;
+export const BorderBottom = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid #ccc;
+  border-bottom: 2px solid #333;
+`;
+export const StyleP = styled.p`
+  display: flex;
+`;
+export const Label = styled.label`
+  font-size: 11px;
+  width: 17%;
+  text-indent: 10px;
+  text-align: left;
+  line-height: 40px;
+  margin-right: 30px;
+`;
+export const LabelInput = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+  width: 70%;
+  text-indent: 5px;
+  line-height: 37px;
+  margin-top: 4px;
+`;
+export const Block = styled.div`
+  margin-top: 5px;
+`;
+export const EmailForm = styled.div`
+  line-height: 30px;
+  display: flex;
+`;
+export const EmailLabel = styled.div`
+  font-size: 11px;
+  width: 17%;
+  text-align: left;
+  text-indent: 10px;
+  line-height: 40px;
+  margin-right: 31px;
+`;
+export const EmailSelect = styled.select`
+  width: 35%;
+  text-align: left;
+  text-indent: 10px;
+  line-height: 40px;
+  margin: 0 31px 0 5px;
+`;
+export const JoinBtn = styled.button`
+  background: #0096d6;
+  color: #fff;
+  padding: 15px 170px;
+  margin-top: 25px;
+  cursor: pointer;
+  &:disabled {
+    cursor: default;
+    background: #dadada;
+    color: white;
+  }
+`;
+export const EmailInput = styled.input`
+  width: 33%;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+  text-indent: 5px;
+  line-height: 37px;
+  margin-right: 5px;
+`;
+export const ValidationJoin = styled.div`
+  color: #ff0000;
+  display: flex;
+  text-indent: 15px;
+  font-size: 10px;
+  margin-top: 5px;
 `;
