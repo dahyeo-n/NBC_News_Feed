4e9c7eec95857d9101ac1cb06ae766515e38c3d8
unknown <qpscm662@naver.com>
2024-02-12 22:14:20 +0900

    feat: mainLogo Route

--- vite-project/src/components/commons/Header.jsx
+++ vite-project/src/components/commons/Header.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Header() {
   return (
     <Parents>
-      <LogoTitle>Header</LogoTitle>
+      <TitleDiv>
+        <Link to={'/'}>Header</Link>
+      </TitleDiv>
     </Parents>
   );
 }
 
 export default Header;
+export const TitleDiv = styled.div`
+  margin-left: 30px;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+`;
 
 export const Parents = styled.div`
   display: flex;
   height: 100px;
   border-bottom: 1px solid #ccc;
   background-color: lightgrey;
-`;
-export const LogoTitle = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 30px;
   font-size: 30px;
 `;
